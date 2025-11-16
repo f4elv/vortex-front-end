0526a84123aabfd177acbba5a1b55dce4e9148bd
Rafael <efael1986@gmail.com>
2025-11-15 23:05:32 -0300

    init of skills page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/sections/hero";
+import Skills from "@/sections/skills";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Skills />
     </main>
   );
 }
